f973996c02aad81821cf7b2ca1e311eabe2e98b3
nicklegr <addr.for+github@gmail.com>
2016-05-08 18:18:59 +0900

    1C A wrong

---
+++ 1C/A.rb
@@ -0,0 +1,135 @@
+require 'pp'
+
+def ppd(*arg)
+  if $DEBUG
+    pp(*arg)
+  end
+end
+
+def putsd(*arg)
+  if $DEBUG
+    puts(*arg)
+  end
+end
+
+def parr(arr)
+  puts arr.join(" ")
+end
+
+def parrd(arr)
+  putsd arr.join(" ")
+end
+
+def ri
+  readline.to_i
+end
+
+def ris
+  readline.split.map do |e| e.to_i end
+end
+
+def rs
+  readline.chomp
+end
+
+def rss
+  readline.chomp.split
+end
+
+def rf
+  readline.to_f
+end
+
+def rfs
+  readline.split.map do |e| e.to_f end
+end
+
+def rws(count)
+  words = []
+  count.times do
+    words << readline.chomp
+  end
+  words
+end
+
+class Integer
+  def popcount32
+    bits = self
+    bits = (bits & 0x55555555) + (bits >>  1 & 0x55555555)
+    bits = (bits & 0x33333333) + (bits >>  2 & 0x33333333)
+    bits = (bits & 0x0f0f0f0f) + (bits >>  4 & 0x0f0f0f0f)
+    bits = (bits & 0x00ff00ff) + (bits >>  8 & 0x00ff00ff)
+    return (bits & 0x0000ffff) + (bits >> 16 & 0x0000ffff)
+  end
+
+  def combination(k)
+    self.factorial/(k.factorial*(self-k).factorial)
+  end
+
+  def permutation(k)
+    self.factorial/(self-k).factorial
+  end
+
+  def factorial
+    return 1 if self == 0
+    (1..self).inject(:*)
+  end
+end
+
+# main
+t_start = Time.now
+
+cases = readline().to_i
+
+(1 .. cases).each do |case_index|
+  n = ri
+  ps = ris
+
+  chars = ("A".."Z").to_a
+
+  ans = []
+  count = ps.inject(:+)
+  while count != 0
+    if count == 2
+      s = ""
+      for i in 0...ps.size
+        if ps[i] != 0
+          s += chars[i]
+        end
+      end
+      ans << s
+      break
+    else
+      raise if count == 1
+
+      max = -1
+      max_i = nil
+      for i in 0...ps.size
+        if max < ps[i]
+          max = ps[i]
+          max_i = i
+        end
+      end
+
+      raise if max == -1 || !max_i
+
+      ans << chars[max_i]
+      ps[max_i] -= 1
+      count -= 1
+    end
+  end
+
+  puts "Case ##{case_index}: #{ans.join(" ")}"
+
+  # progress
+  trigger = 
+    if cases >= 10
+      case_index % (cases / 10) == 0
+    else
+      true
+    end
+
+  if trigger
+    STDERR.puts("case #{case_index} / #{cases}, time: #{Time.now - t_start} s")
+  end
+end
